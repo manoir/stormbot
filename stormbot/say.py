7d55af487eeca8f41c32d76f86d7b97d4a9edfe6
Paul Fariello <pfariello@arkoon.dev>
2017-02-21 14:24:58 +0100

    Change say backend to gtts

--- stormbot/say.py
+++ stormbot/say.py
@@ -1,25 +1,28 @@
 """TTS for stormbot"""
-import os
 import subprocess
+from gtts import gTTS
+from tempfile import NamedTemporaryFile
+
 from .bot import Plugin
 
 
 class Say(Plugin):
     def __init__(self, args):
-        self.player = args.ttf_player
-        self.voice = args.say_voice
+        self.lang = args.say_lang
 
     @classmethod
     def argparse(cls, parser):
-        parser.add_argument("--ttf-player", type=str, default="espeak", help="Say TTF player (default: %(default)s)")
-        parser.add_argument("--say-voice", type=str, default="fr-fr", help="Say voice (default: %(default)s)")
+        parser.add_argument("--say-lang", type=str, default="fr", help="Say lang (default: %(default)s)")
 
     def parser(self, parser):
         subparser = parser.add_parser('say')
         subparser.set_defaults(command=self.run)
-        subparser.add_argument("--voice", type=str, default=self.voice, help="Say voice (default: %(default)s)")
+        subparser.add_argument("--lang", type=str, default=self.lang, help="Say lang (default: %(default)s)")
         subparser.add_argument("text", type=str, help="Text to say")
 
     def run(self, bot, msg, parser, args):
-        cmd = [self.player, '-v', self.voice, args.text]
-        subprocess.Popen(cmd, stdin=None, stdout=None, stderr=None, close_fds=True)
+        tts = gTTS(text=args.text, lang=args.lang)
+        with NamedTemporaryFile() as f:
+            tts.write_to_fp(f)
+            cmd = ['play', '-t', 'mp3', f.name]
+            subprocess.check_call(cmd)
