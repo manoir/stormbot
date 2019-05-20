c8c373c62f53760a9ce54d085fe840b0574d6993
Paul Fariello <paul@fariello.eu>
2019-05-20 21:00:33 +0320

    Try to fix travis docker deployment

--- docker/push.sh
+++ docker/push.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 version=$(grep -Po "(?<=version=([\"']))(([0-9]\.){2}(dev)?[0-9])" setup.py)
 docker build --tag paulfariello/stormbot:$version docker/
